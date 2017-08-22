6ba3c61bdc6a8f6c7d3c2cf30bd03832a469cd80
James Bingo <oliver.saunders@myport.ac.uk>
2017-08-22 20:33:51 +0100

    add configuration

--- watch-converter.py
+++ watch-converter.py
@@ -4,22 +4,35 @@ from watchdog.events import FileSystemEventHandler
 import subprocess
 import datetime
 
+from subprocess import CalledProcessError
+
+import argparse
+
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        subprocess.call(['python','converters.py'])
-        print "Files created in dist at: " + datetime.datetime.now().isoformat()
+        try:
+            subprocess.check_call(['python','converters.py'])
+        except CalledProcessError:
+            # Carry on - error will be propogated 
+            print "\nWatching for changes..."
+        else:
+            print "Files created in dist at: " + datetime.datetime.now().isoformat()
 
 
-if __name__ == "__main__":
-    event_handler = MyHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path='src', recursive=True)
-    observer.start()
+parser = argparse.ArgumentParser(description="Watch stuff")
+parser.add_argument('--path',default='src',dest='path',help="The path to watch")
+args = parser.parse_args()
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, path=args.path, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
